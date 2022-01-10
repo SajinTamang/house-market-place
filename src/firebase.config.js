b712d93319e86aeec10ae4a6d5f8298abcb2bcf0
SajinTamang <sajin.god1.sl@gmail.com>
2022-01-10 23:01:29 +0800

    commit on "House-MarketPlace"

---
+++ src/firebase.config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import{getFirestore} from "firebase/firestore"
+const firebaseConfig = {
+  apiKey: "AIzaSyAf13KduEhA-1un0TX1s8JduscCXFxLCDc",
+  authDomain: "house-marketplace-app-a6174.firebaseapp.com",
+  projectId: "house-marketplace-app-a6174",
+  storageBucket: "house-marketplace-app-a6174.appspot.com",
+  messagingSenderId: "842973916321",
+  appId: "1:842973916321:web:0effcffb8fc5094cf414e0",
+  measurementId: "G-TNNYBDJE42",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const analytics = getAnalytics(app);
+
+export const db = getFirestore()
